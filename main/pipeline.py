b460bf4ce9298563ce33e0dbfe84530f03077cac
secrey <petya.gorodilov@mail.ru>
2024-05-27 19:27:17 +0700

    fix vk response

--- main/pipeline.py
+++ main/pipeline.py
@@ -1,5 +1,17 @@
+# main/pipeline.py
+import logging
+
+logger = logging.getLogger(__name__)
+
 def save_profile(backend, user, response, *args, **kwargs):
     if backend.name == 'vk-oauth2':
-        user.vk_id = response.get('user_id')
-        user.avatar_url = response.get('photo_max')
+        logger.info(f"VK Response: {response}")
+        
+        vk_id = response.get('id')
+        avatar_url = response.get('photo_max_orig')
+
+        logger.info(f"VK ID: {vk_id}, Avatar URL: {avatar_url}")
+
+        user.vk_id = vk_id
+        user.avatar_url = avatar_url if avatar_url else user.avatar_url
         user.save()
--- placesremember/settings.py
+++ placesremember/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'secrey.ru', 'www.secrey.ru']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'secrey.ru', 'secrey.pythonanywhere.com']
 
 # Application definition
 
@@ -118,7 +118,8 @@ AUTHENTICATION_BACKENDS = (
 
 SOCIAL_AUTH_VK_OAUTH2_KEY = env('SOCIAL_AUTH_VK_OAUTH2_KEY')
 SOCIAL_AUTH_VK_OAUTH2_SECRET = env('SOCIAL_AUTH_VK_OAUTH2_SECRET')
-SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+SSOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email', 'photos']
+SOCIAL_AUTH_VK_OAUTH2_EXTRA_DATA = ['photo_max_orig']
 
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
@@ -127,7 +128,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'your_app.pipeline.save_profile',
+    'main.pipeline.save_profile',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
@@ -135,7 +136,7 @@ SOCIAL_AUTH_PIPELINE = (
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
-LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
 
 AUTH_USER_MODEL = 'main.CustomUser'
